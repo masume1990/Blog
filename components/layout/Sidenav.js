5abe0f8ae38c9a4b341450d012c20bb46e340ddf
masume1990 <ma.jafari_2011@yahoo.com>
2022-12-08 12:26:10 +0330

    create structure

---
+++ components/layout/Sidenav.js
@@ -0,0 +1,39 @@
+
+// import { Card,Checkbox,Row,Col } from 'antd';
+// const SideNav = ()=>{
+
+//   const onChangeCategory = ()=>{
+
+//   }
+//     return(
+//       <>
+      
+//       <Row>
+//       <Col span={24}>
+//       <Card
+//     style={{
+//       width: 300,
+//     }}
+//   >
+//     <Row>
+//     <Col span={24}>
+//     <Checkbox onChange={onChangeCategory} style={{direction:"ltr"}}>کامپیوتر</Checkbox>
+//       </Col>
+//       <Col span={24}>
+//     <Checkbox onChange={onChangeCategory} style={{direction:"ltr"}}>کامپیوترکامپیوتر</Checkbox>
+//       </Col>
+//       <Col span={24}>
+//     <Checkbox onChange={onChangeCategory} style={{direction:"ltr"}}>کامپیوترکامپی</Checkbox>
+//       </Col>
+//     </Row>
+   
+    
+//   </Card>
+       
+//         </Col>
+//       </Row>
+
+//   </>
+//     )
+// }
+// export default SideNav;
---
+++ scripts/resources.js
@@ -0,0 +1,7 @@
+export default{
+    enter: "ورود",
+    home:"خانه",
+    blog:"وبلاگ",
+    products:"محصولات",
+    services:"خدمات"
+}
